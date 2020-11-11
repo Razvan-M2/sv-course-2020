df5dc187464536f88179adf7c3ec266dcd9b6039
Razvan <razvanmarc180@yahoo.com>
2020-11-11 19:37:39 +0200

    Introducere simpla

---
+++ main.js
@@ -0,0 +1,16 @@
+var name = "Paul"; // string
+var ammount = 23; // numeric
+var knowJavaScript = true; // boolean
+var fruits = [ "apple", "lemon" , "orange" ]
+
+var person = {
+    height: 2,
+    name: "Julia",
+    canWalk: function(){
+        console.log("Yes, can walk!");
+    }
+}
+
+function add(x,y) {
+    return x + y;
+}
